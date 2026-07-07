ab288353cd62a1411a4f8c4ffd459c4e085d67ab
Dan Navarro <161188343+dannavs@users.noreply.github.com>
2026-07-07 11:42:41 -0700

    Remove unused session import and initialization
    Comment out unused import and session retrieval. Moving from SiS to SiS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -19,8 +19,10 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie', '')
 st.write('The name on your smoothie will be: ', name_on_order)
 
+cnx = st.connection("snowflake")
+session = cnx.session()
+# session = get_active_session()
 
-session = get_active_session()
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
